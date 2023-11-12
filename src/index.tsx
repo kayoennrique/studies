f274aadc5d8e33756d08eec5bd353b0e5f4822e6
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-11-12 17:43:17 -0300

    Using CSS module(index)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.module.scss';
 import App from './pages/App';
 
 const root = ReactDOM.createRoot(
